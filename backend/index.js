17ab736e3bf38484138bc460e03304a08192cae2
Daphne <daphnesofiaglez@gmail.com>
2023-06-15 13:53:41 -0600

    holi

--- backend/index.js
+++ backend/index.js
@@ -90,6 +90,7 @@ app.get('/userreser',requireLogin, (req, res) => {
   res.sendFile(path.join(__dirname, 'public/views/user-reser.html'));
 });
 
+
 app.get('/logout', (req, res) => {
   res.cookie('sessionId', '', { expires: new Date(0) });
   res.redirect('/inicio');
